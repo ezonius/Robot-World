e078bc9afe4581eb19ae4a8af525801f942a60b0
ExoticComet <unoandalfi@icloud.com>
2019-03-21 14:05:23 -0600

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -9,19 +9,19 @@ data:extend({
     type = "double-setting",
     name = "robot-speed-multiplier",
     setting_type = "startup",
-    default_value = 5,
+    default_value = 10,
   },
   {
     type = "double-setting",
     name = "robot-battery-size-multiplier",
     setting_type = "startup",
-    default_value = 0.5,
+    default_value = 0.05,
   },
   {
     type = "double-setting",
     name = "robot-energy-usage-multiplier",
     setting_type = "startup",
-    default_value = 0.2,
+    default_value = 0.02,
   },
   {
     type = "double-setting",
