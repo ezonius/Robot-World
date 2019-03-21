e557eb81b1ce203602858e104a4ce3686cb2b8e1
ExoticComet <unoandalfi@icloud.com>
2019-03-21 14:48:30 -0600

    Update control.lua

--- control.lua
+++ control.lua
@@ -3,9 +3,9 @@ script.on_event(defines.events.on_player_created, function(event)
 		player.insert{name="inserter", count=200}
 		player.insert{name="small-electric-pole", count=200}
 		player.insert{name="electric-mining-drill", count=24}
-		player.insert{name="stone-furnace", count=48}
+		player.insert{name="stone-furnace", count=28}
 		player.insert{name="assembling-machine-1", count=20}
-		player.insert{name="pipe", count=1}
+		player.insert{name="pipe", count=5}
 		player.insert{name="steam-engine", count=8}
 		player.insert{name="boiler", count=4}
 		player.insert{name="offshore-pump", count=1}
@@ -14,7 +14,7 @@ script.on_event(defines.events.on_player_created, function(event)
 		player.insert{name="coal", count=50}
 		player.insert{name="logistic-chest-passive-provider", count=50}
 		player.insert{name="logistic-chest-requester", count=50}
-		player.insert{name="logistic-robot", count=50}
+		player.insert{name="logistic-robot", count=200}
 		player.insert{name="construction-robot", count=50}
 		player.insert{name="roboport", count=10}
 	end)
