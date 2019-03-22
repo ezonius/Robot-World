be6927cb24b385e22037758fd23b2dd98a6df3cf
ExoticComet <unoandalfi@icloud.com>
2019-03-22 08:10:57 -0500

    Update data-final-fixes.lua

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -30,11 +30,17 @@ end
 
 -- Enables recipes
 
-
-for i, v in pairs(data.raw["technology"]["logistic-robotics"]["effects"]) do
-data.raw["recipe"][v.recipe].enabled = true
+if settings.startup["enable-early-logistic-robots"].value then
+	for i, v in pairs(data.raw["technology"]["logistic-robotics"]["effects"]) do
+		ata.raw["recipe"][v.recipe].enabled = true
+	end
 end
-data.raw["recipe"]["construction-robot"].enabled = true
+if settings.startup["enable-early-construction-robots"].value then
+	data.raw["recipe"]["construction-robot"].enabled = true
+	data.raw["recipe"]["roboport"].enabled = true
+	data.raw["recipe"]["logistic-chest-passive-provider"].enabled = true
+	data.raw["recipe"]["logistic-chest-storage"].enabled = true
+end
 if mods["recursive-blueprints"] then
 data.raw["recipe"]["blueprint-deployer"].enabled = true
 data.raw["recipe"]["blueprint-deployer"].ingredients = {
@@ -45,24 +51,31 @@ for i, v in pairs(data.raw["technology"]["circuit-network"]["effects"]) do
 data.raw["recipe"][v.recipe].enabled = true
 end
 end
-for i, v in pairs(data.raw["technology"]["logistic-system"]["effects"]) do
-data.raw["recipe"][v.recipe].enabled = true
+
+if settings.startup["enable-early-logistic-system"].value then
+	for i, v in pairs(data.raw["technology"]["logistic-system"]["effects"]) do
+		data.raw["recipe"][v.recipe].enabled = true
+	end
 end
 
 -- Changes cost of recipes
-
-data.raw["recipe"]["logistic-robot"].ingredients = 
-{
-    {"iron-plate", 1},
-    {"iron-gear-wheel", 1},
-    {"electronic-circuit", 1},
-}           
+if settings.startup["enable-early-logistic-robots"].value then
+	data.raw["recipe"]["logistic-robot"].ingredients = 
+	{
+		{"iron-plate", 1},
+		{"iron-gear-wheel", 1},
+		{"electronic-circuit", 1},
+	}           
+end
+if settings.startup["enable-early-construction-robots"].value then
 data.raw["recipe"]["construction-robot"].ingredients = 
 {
     {"iron-plate", 1},
     {"iron-gear-wheel", 1},
     {"electronic-circuit", 1},
 }
+	end
+if (settings.startup["enable-early-logistic-robots"].value or settings.startup["enable-early-construction-robots"].value) then
 data.raw["recipe"]["logistic-chest-passive-provider"].ingredients = 
 {
                   {"iron-plate", 3},
@@ -73,6 +86,14 @@ data.raw["recipe"]["logistic-chest-storage"].ingredients =
                   {"iron-plate", 3},
                   {"electronic-circuit", 1},
 }
+	data.raw["recipe"]["roboport"].ingredients = 
+{
+  {"iron-plate", 25},
+  {"iron-gear-wheel", 10},
+  {"electronic-circuit", 10}
+}
+	end
+if settings.startup["enable-early-logistic-system"].value then
       for i, v in pairs(data.raw["technology"]["logistic-system"]["effects"]) do
             data.raw["recipe"][v.recipe].ingredients = 
             {
@@ -80,12 +101,8 @@ data.raw["recipe"]["logistic-chest-storage"].ingredients =
                   {"electronic-circuit", 1},
             }
       end
-data.raw["recipe"]["roboport"].ingredients = 
-{
-  {"iron-plate", 25},
-  {"iron-gear-wheel", 10},
-  {"electronic-circuit", 10}
-}
+	end
+
 
 -- Makes electronic-circuits into basic-circuit-boards if bobelectronics is enabled
 
@@ -128,7 +145,7 @@ data.raw["recipe"]["roboport"].ingredients =
 end
 
 -- Adds some new technologies
-
+if (settings.startup["enable-early-logistic-robots"].value or settings.startup["enable-early-construction-robots"].value) then
 data.raw.technology["worker-robots-storage-1"].prerequisites = {"early-worker-robots-storage"}
 data.raw.technology["worker-robots-speed-1"].prerequisites = {"early-worker-robots-speed"}
 
@@ -242,3 +259,5 @@ data:extend({
 
 
 })
+	
+	end
