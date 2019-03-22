5d03f6445192f5dc559e01cbc25df4090027ca0f
ExoticComet <unoandalfi@icloud.com>
2019-03-22 08:05:39 -0500

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -1,25 +1,79 @@
 data:extend({
   {
     type = "double-setting",
+    name = "robot-carry-size-multiplier",
+    setting_type = "startup",
+    default_value = 1,
+  },
+  {
+    type = "double-setting",
     name = "robot-speed-multiplier",
     setting_type = "startup",
-    default_value = 10,
+    default_value = 1,
   },
   {
     type = "double-setting",
     name = "robot-battery-size-multiplier",
     setting_type = "startup",
-    default_value = 0.05,
+    default_value = 1,
   },
   {
     type = "double-setting",
     name = "robot-energy-usage-multiplier",
     setting_type = "startup",
-    default_value = 0.02,
+    default_value = 1,
+  },
+  {
+    type = "double-setting",
+    name = "roboport-logistics-radius-multiplier",
+    setting_type = "startup",
+    default_value = 1
+  },
+  {
+    type = "double-setting",
+    name = "roboport-construction-radius-multiplier",
+    setting_type = "startup",
+    default_value = 1
   },
   {
     type = "double-setting",
     name = "roboport-charging-rate-multiplier",
+    setting_type = "startup",
+    default_value = 1
+  },
+  {
+    type = "double-setting",
+    name = "robot-stack-size",
+    setting_type = "startup",
+    default_value = 50
+  },
+  {
+    type = "double-setting",
+    name = "robot-health-multiplier",
+    setting_type = "startup",
+    default_value = 1
+  },
+  {
+    type = "bool-setting",
+    name = "enable-early-construction-robots",
+    setting_type = "startup",
+    default_value = true
+  },
+  {
+    type = "bool-setting",
+    name = "enable-early-logistic-robots",
+    setting_type = "startup",
+    default_value = true
+  },
+  {
+    type = "bool-setting",
+    name = "enable-early-logistic-system",
+    setting_type = "startup",
+    default_value = true
+  },
+  {
+    type = "double-setting",
+    name = "inserter-speed-multiplier",
     setting_type = "startup",
     default_value = 1
   },
